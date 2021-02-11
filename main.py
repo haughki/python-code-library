486accfd3b80a565e27dca030f8b06c33a530c36
Hawkeye Parker <parker.hawkeye@gmail.com>
2021-02-10 17:23:54 -0800

    misc changes

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import random
 
-character_list = ['&', '\'', '*', '@', '\\', '`', '|', '^', ':', '-', ',', '$', '.', '"', '=', '!', '#', '%', '+', '?', '/', '~', ';', '<', '>', '{', '}', '[', ']', '(', ')']
+t = (1, 2)
+t2 = (6, 5) + t
 
-for i in range(3):
-    list_copy = character_list.copy()
-
-    character_string = ""
-    while len(list_copy) > 0:
-        character_string += ' ' + list_copy.pop(random.randint(0, len(list_copy) - 1))
-
-    print(character_string)
+print(t2)
--- misc/random_string.py
+++ misc/random_string.py
@@ -1,18 +1,25 @@
 import random
 
 # character_list = ['&', '\'', '*', '@', '\\', '`', '|', '^', ':', '-', ',', '$', '.', '"', '=', '!', '#', '%', '+', '?', '/', '~', ';', '<', '>', '{', '}', '[', ']', '(', ')']
-character_list = ['&', '@', '\\', '`', '=', '#', '/', '(', ')']
+# character_list = ['&', '@', '\\', '`', '=', '#', '/', '(', ')']
+character_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+# character_list = ['a', 'b', 'd', 'g', 'h', 'j', 'n', 'r', 'x', 'z']
+# character_list = ['a', 'd', 'h', 'n', 'p', 'x', 'z']
+# character_list = ['the', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog']
 ITERATIONS = 10
-MULTIPLE_LINES = False
+MULTIPLE_LINES = True
+WORDS = False
 
 with open(r'D:\temp\random_string.txt', 'w') as f:
     for i in range(ITERATIONS):
         list_copy = character_list.copy()
 
         character_string = ""
+        space = " " if WORDS else ""
+
         while len(list_copy) > 0:
-            character_string += list_copy.pop(random.randint(0, len(list_copy) - 1))
+            character_string += list_copy.pop(random.randint(0, len(list_copy) - 1)) + space
 
         print(character_string)
-        if MULTIPLE_LINES: character_string + '\n\n'
+        if MULTIPLE_LINES: character_string = character_string + '\n\n'
         f.write(character_string)
